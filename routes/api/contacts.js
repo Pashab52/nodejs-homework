c77ae39ac9a9971686c32fc23ef4fcab35fb2d97
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-09-18 14:46:01 +0300

    t2,t3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,69 +1,15 @@
 const express = require('express')
-const Joi = require("joi");
 const router = express.Router()
+const ctrl = require('../../controllers/contacts')
 
-const contacts = require ('../../models/contacts')
-const {HttpError} = require('../../utils')
+router.get('/', ctrl.getAllContacts);
 
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+router.get('/:contactId', ctrl.getContactById);
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error)
-  }
-  
-})
+router.post('/', ctrl.addContact);
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      throw HttpError(404, "Not found")
-    }
-    res.json(result);
-  } catch (error) {
-      next(error)
-  }
-})
+router.delete('/:contactId', ctrl.delContactById);
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
-     const result = await contacts.addContact(req.body);
-     res.status(201).json(result);
-   } catch (error) {
-     next(error);
-   }
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.editContact(
-      contactId,
-      req.body
-    );
-    res.json(result);
-  } catch (error) {
-    
-  }
-  
-})
+router.put('/:contactId', ctrl.updateContactById)
 
 module.exports = router
